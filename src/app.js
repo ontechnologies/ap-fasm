449f99bb08980b36c3f0597700ba0fbfda35a500
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-08-22 07:02:54 +0200

    Complete Project Setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import morgan from 'morgan'
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlenconded({ extended: true }));
 
